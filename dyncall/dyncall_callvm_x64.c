44421751df149f26caed8ddfc27582d6b3c83702
dadler <dadler@c71cb566-5a31-4692-9165-cf76bdc42cab>
2008-01-22 11:02:18 +0000

    license update
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@72 c71cb566-5a31-4692-9165-cf76bdc42cab

--- dyncall/dyncall_callvm_x64.c
+++ dyncall/dyncall_callvm_x64.c
@@ -1,6 +1,7 @@
 /*/////////////////////////////////////////////////////////////////////////////
 
- Copyright (c) 2007, 2008 Daniel Adler and Tassilo Philipp
+ Copyright (c) 2007, 2008 dyncall project (http://dyncall.org)
+                          Daniel Adler and Tassilo Philipp
 
  Permission to use, copy, modify, and distribute this software for any
  purpose with or without fee is hereby granted, provided that the above
@@ -15,6 +16,7 @@
  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 /////////////////////////////////////////////////////////////////////////////*/
+
 
 /* MS Windows x64 calling convention, AMD64 SystemV ABI. */
 
