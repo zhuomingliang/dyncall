87069d534d41663a597551bcf163473ec82fa325
dadler <dadler@c71cb566-5a31-4692-9165-cf76bdc42cab>
2009-01-14 16:55:37 +0000

    fixed small typo in total report output "suite" -> "suite3"
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@175 c71cb566-5a31-4692-9165-cf76bdc42cab

--- test/suite3/main.cpp
+++ test/suite3/main.cpp
@@ -155,7 +155,7 @@ int main(int argc, char* argv[])
     success = run_range(0,ncalls);
   }
 
-  printf("result: suite: %s\n", success ? "1" : "0");
+  printf("result: suite3: %s\n", success ? "1" : "0");
 
   dcTest_deInitPlatform();
 
