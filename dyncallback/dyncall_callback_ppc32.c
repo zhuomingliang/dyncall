81dcc2b482cca359e6c705421f8f7b1f72600d84
dadler <dadler@c71cb566-5a31-4692-9165-cf76bdc42cab>
2009-06-11 07:08:09 +0000

    added callback dummy for ppc32
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@298 c71cb566-5a31-4692-9165-cf76bdc42cab

---
+++ dyncallback/dyncall_callback_ppc32.c
@@ -0,0 +1,11 @@
+#include "dyncall_callback.h"
+
+DCCallback* dcNewCallback(const char* signature, DCCallbackHandler* handler, void* userdata)
+{
+  return 0;
+}
+
+void dcFreeCallback(DCCallback* pcb)
+{
+}
+
