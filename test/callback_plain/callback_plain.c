dcaab419aadfed88a07a55b2f9869dce1bc21644
tphilipp <tphilipp@c71cb566-5a31-4692-9165-cf76bdc42cab>
2009-11-14 01:13:03 +0000

    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@454 c71cb566-5a31-4692-9165-cf76bdc42cab

---
+++ test/callback_plain/callback_plain.c
@@ -0,0 +1,31 @@
+#include "../common/platformInit.h"
+#include "../../dyncallback/dyncall_callback.h"
+#include <stdio.h>
+
+char cbHandler(DCCallback* cb, DCArgs* args, DCValue* result, void* userdata)
+{
+  (*(int*)userdata) = 1;
+  result->s = 1234;
+  return 'i';		// @@@ support needed for 's', etc
+}
+
+
+int main()
+{
+  DCCallback* cb;
+  short result = 0;
+  int success = 0;
+
+  dcTest_initPlatform();
+
+  cb = dcNewCallback("if)s", &cbHandler, &success);
+  result = ((short(*)(int, float))cb)(123, 23.f);
+  dcFreeCallback(cb);
+
+  printf("result: callback_suite: %s\n", success && (result == 1234) ? "1" : "0");
+
+  dcTest_deInitPlatform();
+
+  return 0;
+}
+
