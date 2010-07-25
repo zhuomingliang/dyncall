632cfdf9fa3d8e0416c93f0fbfc7b80f67be8466
tphilipp <tphilipp@c71cb566-5a31-4692-9165-cf76bdc42cab>
2010-07-25 22:46:49 +0000

    - some more plan9 build stuff and assembly conversion (doesn't work, though, but on the right track)
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@677 c71cb566-5a31-4692-9165-cf76bdc42cab

--- buildsys/mk/pcc.mk
+++ buildsys/mk/pcc.mk
@@ -27,9 +27,28 @@ LD = pcc
 #@@@ add -g option or similar
 #@@@.endif
 CPPFLAGS = -D__Plan9__ -D__${objtype}__
-CFLAGS   = $CFLAGS -D__Plan9__ -D__${objtype}__ -I$TOP/dyncall -I$TOP/dyncallback -c
+CFLAGS   = -D__Plan9__ -D__${objtype}__ -I$TOP/dyncall -I$TOP/dyncallback -c
 #CXXFLAGS = $CXXFLAGS -D__Plan9__
 #ASFLAGS  = -D__Plan9__
+
+# JUST TEMPORARY AS LONG AS I'M WORKING ON IT - Plan9's sed doesn't support sed comments
+#        /^$/d                                                               # Remove empty lines.
+#        /^#.*/d                                                             # Remove comments.
+#        s/^\.(globl|intel_syntax|file|section).*//                          # Remove some GAS directives.
+#        s/(.*):/TEXT \1(SB), $0/g                                           # Reformat function names.
+#        s/%//g                                                              # Remove % register prefixes.
+#        /^[  ]+/y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/    # Convert everything to uppercase.
+#        s/([A-Z]+)[  ]*E(..)[,   ]*E(..)/\1L \3, \2/                        # Convert 32bit instructions with 2 register operands.
+#        s/([A-Z]+)[  ]*E(..)[,   ]*([0-9]+)/\1L $\3, \2/                    # Convert 32bit instructions with register and constant as
+#        s/([A-Z]+)[  ]*E(..)[,   ]*\[E(..)\+([0-9]+)\]/\1L \4(\3), \2/      # Convert 32bit instructions with register and address as 
+#        s/([A-Z]+)[  ]*E(..)[,   ]*\[E(..)\]/\1L 0(\3), \2/                 # Convert 32bit instructions with register and address as 
+#        s/(CALL)[    ]*E(..)[    ]*$/\1 \2/                                 # Convert CALL instructions.
+#        s/(CALL)[    ]*\[E(..)\+([0-9]+)\]/\1 \3(\2)/                       # Convert CALL instructions with addressing mode.
+#        s/([A-Z]+)[  ]*E(..)[    ]*$/\1L \2/                                # Convert 32bit instructions with register operand.
+#        s/([A-Z]+)[  ]*\[E(..)\+([0-9]+)\]/\1L \3(\2)/                      # Convert 32bit instructions with address operand.
+#        s/([A-Z]+)[  ]*\[E(..)\]/\1L 0(\2)/                                 # Convert 32bit instructions with address operand.
+#        s/(REP)[     ]*(MOV)SB[  ]*$/\1; \2B SI, DI/                        # Convert repeater prefix stuff.
+#        s/(REP)[     ]*(MOV)SD[  ]*$/\1; \2L SI, DI/                        # Convert repeater prefix stuff.
 
 # Step to transform .S into object files.
 %.$O: %.S
@@ -42,7 +61,7 @@ CFLAGS   = $CFLAGS -D__Plan9__ -D__${objtype}__ -I$TOP/dyncall -I$TOP/dyncallbac
 		s/%//g
 		/^[	 ]+/y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/
 		s/([A-Z]+)[	 ]*E(..)[,	 ]*E(..)/\1L \3, \2/
-		s/([A-Z]+)[	 ]*E(..)[,	 ]*([0-9]+)/\1L \3, \2/
+		s/([A-Z]+)[	 ]*E(..)[,	 ]*([0-9]+)/\1L $\3, \2/
 		s/([A-Z]+)[	 ]*E(..)[,	 ]*\[E(..)\+([0-9]+)\]/\1L \4(\3), \2/
 		s/([A-Z]+)[	 ]*E(..)[,	 ]*\[E(..)\]/\1L 0(\3), \2/
 		s/(CALL)[	 ]*E(..)[	 ]*$/\1 \2/
