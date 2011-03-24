ca53ff78ddc7c3afdd39e1e723ef316e48431bc5
dadler <dadler@c71cb566-5a31-4692-9165-cf76bdc42cab>
2011-03-24 01:36:24 +0000

    add syscall test
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@827 c71cb566-5a31-4692-9165-cf76bdc42cab

---
+++ test/syscall/syscall.c
@@ -0,0 +1,26 @@
+#include "dyncall.h"
+#include <sys/syscall.h> 
+DCCallVM* callvm;
+
+void syscallvm_init()
+{
+  callvm = dcNewCallVM(4096);
+  dcMode(callvm, DC_CALL_SYS_DEFAULT);
+}
+
+int syscall_write(int fd, char* buf, size_t len)
+{
+  dcReset(callvm);
+  dcArgInt(callvm, fd);
+  dcArgPointer(callvm, buf);
+  dcArgInt(callvm, len);
+  return dcCallInt(callvm, (DCpointer) (ptrdiff_t) SYS_write);
+}
+
+int main(int argc, char* argv[])
+{
+  syscallvm_init();
+  syscall_write(1, "hello\n", 6 );
+  return 0;
+}
+
