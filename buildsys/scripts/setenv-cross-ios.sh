617dd63a5dddd43d5648efc2498e8b9080f6cf3d
dadler <dadler@c71cb566-5a31-4692-9165-cf76bdc42cab>
2011-05-03 12:00:48 +0000

    docu updates and upgrade to 4.3 sdk for iphone
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@1079 c71cb566-5a31-4692-9165-cf76bdc42cab

--- buildsys/scripts/setenv-cross-ios.sh
+++ buildsys/scripts/setenv-cross-ios.sh
@@ -1,6 +1,6 @@
 
 IOS_ARCH="armv6"
-IOS_SDK_VERSION="3.2" 
+IOS_SDK_VERSION="4.3" 
 
 export TARGET_MACH="-arch ${IOS_ARCH}" 
 export TARGET_ARCH="-arch ${IOS_ARCH} -isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${IOS_SDK_VERSION}.sdk" 
