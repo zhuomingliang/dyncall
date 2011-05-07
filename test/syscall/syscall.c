6c56c14be79a0ee4e195acad73e43a95c6641384
tphilipp <tphilipp@c71cb566-5a31-4692-9165-cf76bdc42cab>
2011-05-07 18:51:03 +0000

    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@1105 c71cb566-5a31-4692-9165-cf76bdc42cab

--- test/syscall/syscall.c
+++ test/syscall/syscall.c
@@ -14,13 +14,13 @@ int syscall_write(int fd, char* buf, size_t len)
   dcArgInt(callvm, fd);
   dcArgPointer(callvm, buf);
   dcArgInt(callvm, len);
-  return dcCallInt(callvm, (DCpointer) (ptrdiff_t) SYS_write);
+  return dcCallInt(callvm, (DCpointer)(ptrdiff_t)SYS_write);
 }
 
 int main(int argc, char* argv[])
 {
   syscallvm_init();
-  syscall_write(1, "hello\n", 6 );
+  syscall_write(1/*stdout*/, "syscall: result: 1\n", 19);
   return 0;
 }
 
