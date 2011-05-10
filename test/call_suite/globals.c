df83c917f1b274c329521c93d81ca1da1dffedaf
tphilipp <tphilipp@c71cb566-5a31-4692-9165-cf76bdc42cab>
2011-05-10 15:51:57 +0000

    - Plan9 build fixes - added shell-based callf tool
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@1132 c71cb566-5a31-4692-9165-cf76bdc42cab

--- test/call_suite/globals.c
+++ test/call_suite/globals.c
@@ -22,8 +22,8 @@ DEF_TYPES
     K_j[i] = (long)      (rand_d() * (1L<<(sizeof(long)*8-1)));
     K_l[i] = (long long) (rand_d() * (1LL<<(sizeof(long long)*8-1)));
     K_p[i] = (void*)     (long) (rand_d() * (1L<<(sizeof(void*)*8-1)));
-    K_f[i] = (float)     (rand_d() * __FLT_MAX__);
-    K_d[i] = (double)    (rand_d() * __DBL_MAX__);
+    K_f[i] = (float)     (rand_d() * 3.40282347E+38F/*__FLT_MAX__*/);	/* Plan9 doesn't know the macro. */
+    K_d[i] = (double)    (rand_d() * 1.7976931348623157E+308/*__DBL_MAX__*/);	/* Plan9 doesn't know the macro. */
   }
 }
 
