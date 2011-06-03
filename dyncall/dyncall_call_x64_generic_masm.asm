576fc0a801c8f66f28c0ec762cc6e6ebf1a80a90
dadler <dadler@c71cb566-5a31-4692-9165-cf76bdc42cab>
2011-06-03 08:45:44 +0000

    updates from win64 for at&t style portasm status: call_suite works fine fixes for dyncall Nmakefile had to fix call_suite using FLT_MAX again (please check again on Plan9 if this works!)
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@1145 c71cb566-5a31-4692-9165-cf76bdc42cab

--- dyncall/dyncall_call_x64_generic_masm.asm
+++ dyncall/dyncall_call_x64_generic_masm.asm
@@ -4,33 +4,31 @@ dcCall_x64_sysv PROC
 OPTION PROLOGUE:NONE, EPILOGUE:NONE
  push RBP
  push RBX
- mov RBP, RSP
- mov RBX, R8
- movsd XMM0, qword ptr[RCX ]
- movsd XMM1, qword ptr[RCX+ 8]
- movsd XMM2, qword ptr[RCX+16]
- movsd XMM3, qword ptr[RCX+24]
- movsd XMM4, qword ptr[RCX+32]
- movsd XMM5, qword ptr[RCX+40]
- movsd XMM6, qword ptr[RCX+48]
- movsd XMM7, qword ptr[RCX+56]
- sub RSP, RDI
- mov RAX, RDI
- add RAX, 8
- and RAX, 15
- sub RSP, RAX
- mov RCX, RDI
- mov RDI, RSP
+ mov RBP,RSP
+ mov RBX,R8
+ movsd XMM0,qword ptr [RCX+0]
+ movsd XMM1,qword ptr [RCX+8]
+ movsd XMM2,qword ptr [RCX+16]
+ movsd XMM3,qword ptr [RCX+24]
+ movsd XMM4,qword ptr [RCX+32]
+ movsd XMM5,qword ptr [RCX+40]
+ movsd XMM6,qword ptr [RCX+48]
+ movsd XMM7,qword ptr [RCX+56]
+ add RDI,31
+ and RDI,-32
+ sub RSP,RDI
+ mov RCX,RDI
+ mov RDI,RSP
  rep movsb
- mov RDI, qword ptr[RDX ]
- mov RSI, qword ptr[RDX+ 8]
- mov RCX, qword ptr[RDX+24]
- mov R8, qword ptr[RDX+32]
- mov R9, qword ptr[RDX+40]
- mov RDX, qword ptr[RDX+16]
- mov AL, 8
+ mov RDI,qword ptr [RDX+0]
+ mov RSI,qword ptr [RDX+8]
+ mov RCX,qword ptr [RDX+24]
+ mov R8,qword ptr [RDX+32]
+ mov R9,qword ptr [RDX+40]
+ mov RDX,qword ptr [RDX+16]
+ mov AL,8
  call RBX
- mov RSP, RBP
+ mov RSP,RBP
  pop RBX
  pop RBP
  ret
@@ -40,28 +38,28 @@ OPTION PROLOGUE:NONE, EPILOGUE:NONE
  push RBP
  push RSI
  push RDI
- mov RBP, RSP
- add RCX, 15
- and RCX, -16
- sub RSP, RCX
- mov RSI, RDX
- mov RDI, RSP
- mov RAX, R9
+ mov RBP,RSP
+ add RCX,15
+ and RCX,-16
+ sub RSP,RCX
+ mov RSI,RDX
+ mov RDI,RSP
+ mov RAX,R9
  rep movsb
- mov RCX, qword ptr[R8 ]
- mov RDX, qword ptr[R8+ 8]
- mov R9, qword ptr[R8+24]
- mov R8, qword ptr[R8+16]
- movd XMM0, RCX
- movd XMM1, RDX
- movd XMM2, R8
- movd XMM3, R9
+ mov RCX,qword ptr [R8+0]
+ mov RDX,qword ptr [R8+8]
+ mov R9,qword ptr [R8+24]
+ mov R8,qword ptr [R8+16]
+ movd XMM0,RCX
+ movd XMM1,RDX
+ movd XMM2,R8
+ movd XMM3,R9
  push R9
  push R8
  push RDX
  push RCX
  call RAX
- mov RSP, RBP
+ mov RSP,RBP
  pop RDI
  pop RSI
  pop RBP
--- test/call_suite/globals.c
+++ test/call_suite/globals.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "globals.h"
+#include <float.h>
 
 #define X(CH,T,QCH) T *V_##CH; T *K_##CH; 
 DEF_TYPES
@@ -11,10 +12,10 @@ double rand_d() { return ( ( (double) rand() )  / ( (double) RAND_MAX ) ); }
 
 void init_K()
 {
+  int i;
 #define X(CH,T,QCH) V_##CH = (T*) malloc(sizeof(T)*G_maxargs); K_##CH = (T*) malloc(sizeof(T)*G_maxargs);
 DEF_TYPES
 #undef X
-  int i;
   for(i=0;i<G_maxargs;++i) {
     K_c[i] = (char)      (rand_d() * (1<<7));
     K_s[i] = (short)     (rand_d() * (1<<(sizeof(int)*8-1)));
@@ -22,7 +23,7 @@ DEF_TYPES
     K_j[i] = (long)      (rand_d() * (1L<<(sizeof(long)*8-1)));
     K_l[i] = (long long) (rand_d() * (1LL<<(sizeof(long long)*8-1)));
     K_p[i] = (void*)     (long) (rand_d() * (1L<<(sizeof(void*)*8-1)));
-    K_f[i] = (float)     (rand_d() * 3.40282347E+38F/*__FLT_MAX__*/);	/* Plan9 doesn't know the macro. */
+    K_f[i] = (float)     (rand_d() * FLT_MAX);	/* Plan9 doesn't know the macro. */
     K_d[i] = (double)    (rand_d() * 1.7976931348623157E+308/*__DBL_MAX__*/);	/* Plan9 doesn't know the macro. */
   }
 }
