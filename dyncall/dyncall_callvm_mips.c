246d32816f3331f58b0b66176a8ba7b02292e040
tphilipp <tphilipp@c71cb566-5a31-4692-9165-cf76bdc42cab>
2011-08-13 16:16:41 +0000

    -cosmetics
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@1181 c71cb566-5a31-4692-9165-cf76bdc42cab

--- dyncall/dyncall_callvm_mips.c
+++ dyncall/dyncall_callvm_mips.c
@@ -1,10 +1,10 @@
 #if defined(__GNUC__)
 
-#if defined(_ABIO32)
+#if defined(DC__ABI_MIPS_O32)
 #include "dyncall_callvm_mips_o32.c"
-#elif defined(_ABI64)
+#elif defined(DC__ABI_MIPS_N64)
 #include "dyncall_callvm_mips_n64.c"
-#elif defined(_ABIN32)
+#elif defined(DC__ABI_MIPS_N32)
 #include "dyncall_callvm_mips_n32.c"
 #else
 #include "dyncall_callvm_mips_eabi.c"
