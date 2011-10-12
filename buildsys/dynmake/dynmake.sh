5bddd844c4e1d08df733b9b1a6c0643cda3614c1
tphilipp <tphilipp@c71cb566-5a31-4692-9165-cf76bdc42cab>
2011-10-12 23:24:03 +0000

    - fixes to dynmake to keep currently used "make" for recursive builds
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@1268 c71cb566-5a31-4692-9165-cf76bdc42cab

--- buildsys/dynmake/dynmake.sh
+++ buildsys/dynmake/dynmake.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #cpp -P Makefile.M | sed "s/^  */	/" > Makefile.dynmake
 gcc -E -P -x c Makefile.M | sed "s/^  */	/" > Makefile.dynmake
-make -f Makefile.dynmake $1
+$2 -f Makefile.dynmake $1
