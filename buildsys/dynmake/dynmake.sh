2c7eb5f11b31fc6267e09224955da1e2832598af
tphilipp <tphilipp@c71cb566-5a31-4692-9165-cf76bdc42cab>
2011-10-12 01:20:20 +0000

    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@1266 c71cb566-5a31-4692-9165-cf76bdc42cab

--- buildsys/dynmake/dynmake.sh
+++ buildsys/dynmake/dynmake.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-cpp -P Makefile.M | sed "s/^  */	/" > Makefile.dynmake
+#cpp -P Makefile.M | sed "s/^  */	/" > Makefile.dynmake
+gcc -E -P -x c Makefile.M | sed "s/^  */	/" > Makefile.dynmake
 make -f Makefile.dynmake $1
