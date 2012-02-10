dc0fab1c09634cd5571bf0a0deefd44c75dd554e
tphilipp <tphilipp@c71cb566-5a31-4692-9165-cf76bdc42cab>
2012-02-10 03:04:40 +0000

    - PSP fixes
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@1344 c71cb566-5a31-4692-9165-cf76bdc42cab

--- test/common/platformInit.h
+++ test/common/platformInit.h
@@ -65,6 +65,19 @@ void dcTest_deInitPlatform()
 
 #elif defined(DC__OS_PSP)
 
+#include <pspkernel.h>
+#include <pspiofilemgr.h>
+#include <pspmodulemgr.h>
+#include <pspdisplay.h>
+#include <pspdebug.h>
+#include <pspusb.h>
+#include <pspusbstor.h>
+#include <pspthreadman.h>
+#include <pspctrl.h>
+#include <pspsdk.h>
+
+PSP_MODULE_INFO("dyncall_test",0,1,1);
+
 /*  
 int exit_callback(int arg1, int arg2, void* common) 
 {
