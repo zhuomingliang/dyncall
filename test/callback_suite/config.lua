a952f463e156f13e0c840d1e1e9ecedd4a16210d
tphilipp <tphilipp@c71cb566-5a31-4692-9165-cf76bdc42cab>
2013-10-22 00:01:10 +0000

    - limited default number of generated callback_suite function parameters to 29, to make Plan9's pcc happy
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@1480 c71cb566-5a31-4692-9165-cf76bdc42cab

--- test/callback_suite/config.lua
+++ test/callback_suite/config.lua
@@ -1,10 +1,10 @@
 -- auto-generated file from temlate config-random.lua (created by GNUmake)
 -- section 'main' --------------------------------------------------------------
                   
-nsigs     = 300         -- number of test cases
+nsigs     = 400         -- number of test cases
 mode      = "random"    -- generatore mode: 'random' or 'ordered' type sequences
-minargs   = 1           -- minimum num. of supported arguments (>= 0)
-maxargs   = 67          -- maximum num. of supported arguments (>= minargs)
+minargs   = 0           -- minimum num. of supported arguments (>= 0)
+maxargs   = 29          -- maximum num. of supported arguments (>= minargs)
 
 
 -- section 'types' -------------------------------------------------------------
