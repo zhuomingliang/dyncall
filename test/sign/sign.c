6cfc669b2fe3f86a4973bf841f7d822a4cae7001
dadler <dadler@c71cb566-5a31-4692-9165-cf76bdc42cab>
2015-01-17 01:13:28 +0000

    updated sign test (switching to 'short' test)
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@1665 c71cb566-5a31-4692-9165-cf76bdc42cab

--- test/sign/sign.c
+++ test/sign/sign.c
@@ -22,10 +22,24 @@
    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 */
+
 #include "dyncall.h"
 #include <stdio.h>
 
-int add1(unsigned char x) { return x + 1; }
+// This program demonstrates the need for 'unsigned' integers and the
+// ability to implement zero/sign extensions for small integers.
+// This test program indicates failure on powerpc 32-bit with
+// compiler optimizations enabled (e.g. '-O2' compilation flags).
+//
+// A fix to be incorporated in the API is also given.
+//
+
+int add1s(unsigned short x) { return x + 1; }
+
+// API BUGFIX:
+#define dcArgUShort(vm,x) dcArgInt(vm,(int)(unsigned int)(x))
+
+// OLD TEST: int add1(unsigned char x) { return x + 1; }
 
 int main(int argc, char* argv[])
 {
@@ -34,9 +48,31 @@ int main(int argc, char* argv[])
   int total;
   vm = dcNewCallVM(4096);
   dcReset(vm);
-  dcArgChar( vm, (char) 255 );
-  result = dcCallInt( vm, &add1 );
-  total = (result == 256);
+
+  dcArgShort(vm, 0xFFFF );
+  result = dcCallInt( vm, &add1s );
+  total = (result == 0x10000);
   printf("result: sign: %d\n", total); 
+ 
+  if (!total) {
+    //
+    // TEST BUGFIX: use instead..
+    //
+    dcReset(vm);
+    dcArgUShort( vm, 0xFFFF );
+    result = dcCallInt( vm, &add1s );
+    total = (result == 0x10000);
+    printf("result: sign (bugfix): %d\n", total); 
+  }
+  
+  // result = dcCallInt( vm, &add1s );
+  // total = (result == 0x10000);
+  
+  // OLD TEST: updated to using 'short'
+  // dcArgChar( vm, (char) 255 );
+  // result = dcCallInt( vm, &add1 );
+  // total = (result == 256);
+  
+  // printf("result: sign: %d\n", total); 
   return 0;
 }
