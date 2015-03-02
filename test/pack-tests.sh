9575fb7e42d6f6dafcc4919d363e2a09969caaa8
dadler <dadler@c71cb566-5a31-4692-9165-cf76bdc42cab>
2015-03-02 16:08:27 +0000

    added test/pack-test.sh tool
    git-svn-id: http://dyncall.org/svn/dyncall/trunk/dyncall@1721 c71cb566-5a31-4692-9165-cf76bdc42cab

---
+++ test/pack-tests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# script to package some tests for quick deployment (useful for embedded stuff)
+
+TESTS="callf ellipsis malloc_wx plain plain_c++ suite suite2 suite3 suite_floats callback_plain callback_suite"
+# addition test:
+#
+# callback_plain callback_suite callf
+# ellipsis
+# malloc_wx thunk
+# nm
+# plain suite suite2 suite3
+# suite2_x86win32fast suite2_x86win32std suite_x86win32fast suite_x86win32std
+
+DATE=`date +%s`
+NAME="dyncall-tests-${DATE}"
+
+mkdir -p _work/${NAME}
+for I in $TESTS ; do
+cp $I/$I _work/${NAME}
+done
+mkdir -p _packed
+tar -cvzf _packed/${NAME}.tar.gz -C _work ${NAME}
